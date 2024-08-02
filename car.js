b185ca06a45a0f76d336b6427cab9584564f1285
Adhvet <sharmaadhvet@gmail.com>
2024-08-03 01:29:18 +0530

    artificial sensors added

--- car.js
+++ car.js
@@ -11,13 +11,15 @@ class Car{
         this.friction=0.05;
         this.angle =0;
 
+       this.sensor = new Sensor(this);
         this.controls=new Controls();
     }
 
-    update(){
+    update(roadBorders){
         this.#move();
+        this.sensor.update(roadBorders);
     }
-
+    
     #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration
@@ -72,5 +74,7 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
